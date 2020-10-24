9117dd55c95a9c81864c1aa005e169cb731299e4
tanviredu <ornobtanvir.git@gmail.com>
2020-10-25 00:54:40 +0600

    ADDING INTERFACE AND IMPLEMENTATION

---
+++ PieShop.Data/Implementation/CountryRepository.cs
@@ -0,0 +1,31 @@
+﻿using PieShop.Data.RepositoryInterface;
+using PieShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PieShop.Data.Implementation
+{
+    public class CountryRepository : ICountryRepository
+    {
+        private readonly AppDbContext _appDbContext;
+
+        // the constructor is for  when the class
+        // will init it will already get a dbContext
+        // connection
+        public CountryRepository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public IEnumerable<Country> GetAllCountries()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Country GetCountryById(int countryId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
