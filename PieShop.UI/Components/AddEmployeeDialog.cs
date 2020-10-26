773bef526168becb3bb29ff5d1a534736208ae6d
tanviredu <ornobtanvir.git@gmail.com>
2020-10-27 03:41:30 +0600

    COMPONENTS ARE ADDED

---
+++ PieShop.UI/Components/AddEmployeeDialog.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PieShop.Models;
+using PieShop.Data;
+using Microsoft.AspNetCore.Components;
+using PieShop.Data.Implementation;
+
+
+
+/*
+ * THIS IS A COMPONENT SO WE NEED TO IMPORT
+ * THE COMPONENT BASE FROM ASP.NET CORE
+ * AND THERE WILL BE NO URL ONLY RHE UI CODE
+ * AND THE LOGIC CODE IN A PARTIAL CLASS
+ *  WE USE A FUNCTION NAME StatehasChanged()
+ *  THAT WILL RESET THE STATE 
+ */
+
+namespace PieShop.UI.Components
+{
+    public partial class AddEmployeeDialog:ComponentBase
+    {
+        public EmployeeRepository employeeRepository;
+        public Employee Employee { get; set; }
+        public bool ShowDialog { get; set; }
+        private void ResetDialog()
+        {
+            // this method give a default data 
+            // when the state has changed in add Employee component
+
+            Employee = new Employee { 
+                CountryId = 1,
+                JobCategoryId = 1,
+                BirthDate = DateTime.Now,
+                JoinedDate = DateTime.Now 
+            };
+
+        }
+        public void Close() {
+            // when user close the dialog
+            ShowDialog = false;
+            StateHasChanged(); // this is a default function
+        }
+
+        public void Show() {
+
+            ResetDialog();
+            ShowDialog = true;
+            StateHasChanged();
+            
+        }
+
+        protected void HandleValidSubmit() {
+            employeeRepository.AddEmployee(Employee);
+            ShowDialog = false;
+            
+        }
+
+
+    }
+
+    
+
+}
---
+++ PieShop.UI/Components/NewEmployeesWidget.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PieShop.Models;
+using PieShop.Data;
+using Microsoft.AspNetCore.Components;
+using PieShop.Data.Implementation;
+
+namespace PieShop.UI.Components
+{
+    public partial class NewEmployeesWidget : ComponentBase
+    {
+
+        public EmployeeRepository employeeRepository;
+        public List<Employee> NewEmployees { get; set; } = new List<Employee>();
+
+
+        protected override void OnInitialized()
+        {
+            NewEmployees = employeeRepository.GetAllEmployees().OrderBy(x => x.JoinedDate).Take(3).ToList();
+
+        }
+    }
+}
